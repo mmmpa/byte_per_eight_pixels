4b29311213875eaba1c624207361abb16155c26d
mmmpa <mmmpa.mmmpa@gmail.com>
2020-08-01 15:09:15 +0900

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 #![feature(stmt_expr_attributes)]
 
+mod common;
 mod eight_px_uint_eight;
 mod error;
 mod vertical_eight_px_uint_eight;
 
 pub use crate::eight_px_uint_eight::*;
+pub use common::*;
 pub use error::*;
 pub use vertical_eight_px_uint_eight::*;
 
